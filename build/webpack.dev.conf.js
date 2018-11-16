c8ba51564c84198118187c9bfdd4b328ae7c4fdb
Shen Hao <775308609@qq.com>
2018-11-16 17:41:50 +0800

    内网穿透设置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -23,6 +23,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   // these devServer options should be customized in /config/index.js
   devServer: {
     clientLogLevel: 'warning',
+    disableHostCheck: true, //内网穿透的设置
     historyApiFallback: {
       rewrites: [
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
